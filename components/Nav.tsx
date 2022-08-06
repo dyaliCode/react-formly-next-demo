2919ef6b5dedc3f7e4bbe7ab75d4d795f5537a56
kamalkech <maroc.develop@gmail.com>
2022-08-06 14:04:08 +0100

    .

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,11 +13,14 @@ const Nav = () => {
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link href="/">
           <a className="flex items-center">
-            <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
+            <div className="mr-3 h-6 sm:h-9 relative">
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+                alt="Flowbite Logo"
+                width="50%"
+                height="35%"
+              />
+            </div>
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Formly React
             </span>
